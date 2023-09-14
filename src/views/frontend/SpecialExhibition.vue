c44ff5dece000de28303a6c337a903570ca3863e
Jeff Chiu <jiefuchiu@gmail.com>
2023-09-14 16:45:16 +0800

    jeff_update special exhibition

---
+++ src/views/frontend/SpecialExhibition.vue
@@ -0,0 +1,14 @@
+<template>
+    <input type="search" id="searchBtn" name="searchBtn" placeholder="Search &#128269;">
+    <div class="exhi_section">
+        <div class="exhi_img">
+            <img src="./specialExhibition_image/sing_in_spring.jpg" alt="">
+        </div>
+        <div class="exhi_desc">
+            <span>2024/02/01-03/30</span>
+            <h3>春之詠歌：多彩藝術的季節</h3>
+            <p>來自各地藝術家們帶來的多種形式的作品，包括繪畫、雕塑、攝影、錄像藝術、裝置藝術和互動藝術。透過不同的媒介和風格，試圖捕捉春天所帶來的生命力和變化。</p>
+        </div>
+        <div class="exhi_desc_bgc"></div>
+    </div>
+</template>
