2d94cf4c4728c9f65cc712d1ccf3e3a01d040faa
helilun <tracy021724@gmail.com>
2023-09-14 13:41:48 +0800

    shopping_upgrade_1R

--- src/views/frontend/Login.vue
+++ src/views/frontend/Login.vue
@@ -75,7 +75,7 @@ export default {
 };
 </script>
 <style scoped lang="scss">
-@import "../assets/sass/style.scss";
+@import "@/assets/sass/style.scss";
 main {
   min-height: calc(100vh - 130px);
   margin: auto;
--- src/views/frontend/Shop.vue
+++ src/views/frontend/Shop.vue
@@ -7,13 +7,13 @@
     class="shop_carousel"
   >
     <CarouselItem class="banner">
-      <img src="../assets/image/productimage/banner/AntoinettePoudre.png" />
+      <img src="@/assets/image/productimage/banner/AntoinettePoudre.png" />
     </CarouselItem>
     <CarouselItem class="banner">
-      <img src="../assets/image/productimage/banner/Venuslipstick.png" />
+      <img src="@/assets/image/productimage/banner/Venuslipstick.png" />
     </CarouselItem>
     <CarouselItem class="banner">
-      <img src="../assets/image/productimage/banner/Louise-Maske.png" />
+      <img src="@/assets/image/productimage/banner/Louise-Maske.png" />
     </CarouselItem>
   </Carousel>
   <h1>熱銷商品</h1>
@@ -23,33 +23,41 @@
       <a href="">商城 /</a>
       <a href="">熱銷商品</a>
     </div>
-    <select class="shop_filter">
-      <option>熱門排行</option>
-      <option>價格低至高</option>
-      <option>價格高至低</option>
-      <option>名稱筆畫</option>
-    </select>
+    <div class="search">
+      <img src="@/assets/image/productimage/heart.png" alt="">
+      <img src="@/assets/image/productimage/shoppingcar.png" alt="">
+      <label for="search"></label>
+      <form action="">
+        <input
+          type="text"
+          placeholder="Search"
+          id="search"
+          v-model="searchinput"
+        />
+        <button type="button" @click="searchClick">
+          <i class="fa-solid fa-magnifying-glass"></i>
+        </button>
+        <!-- <button type="button" @click="reset">
+          <font-awesome-icon :icon="['fas', 'rotate-right']" />
+        </button> -->
+      </form>
+    </div>
   </div>
   <div class="shop_wrap">
     <aside class="shop_aside">
-      <router-link :to="a.link" v-for="a in aside">{{ a.name }}</router-link>
-      <div class="search">
-        <label for="search">搜尋商品</label>
-        <form action="">
-          <input
-            type="text"
-            placeholder="輸入商品..."
-            id="search"
-            v-model="searchinput"
-          />
-          <button type="button" @click="searchClick">
-            <i class="fa-solid fa-magnifying-glass"></i>
-          </button>
-          <button type="button" @click="reset">
-            <font-awesome-icon :icon="['fas', 'rotate-right']" />
-          </button>
-        </form>
-      </div>
+      <router-link :to="a.link" v-for="a in aside">{{ a.name }}
+      </router-link>
+      <select class="shop_filter">
+        <option>
+          商品排序
+        </option>
+        <option>
+          價格低至高
+        </option>
+        <option>
+          價格高至低
+        </option>
+      </select>
     </aside>
     <div class="shop_container">
       <div v-if="produstdisplay == 0">
@@ -60,7 +68,7 @@
           <div class="image">
             <img
               :src="
-                require('../assets/image/productimage/productimage' +
+                require('@/assets/image/productimage/productimage' +
                   index +
                   '.png')
               "
@@ -68,8 +76,12 @@
             />
           </div>
           <div class="info">
-            <span>{{ item.title }}</span>
-            <span>{{ item.price }}</span>
+            <span>
+              <img src="@/assets/image/productimage/heart.png" alt="">
+              {{item.title}}
+              <img src="@/assets/image/productimage/shoppingcar.png" alt="">
+            </span>
+            <span>${{item.price}}</span>
           </div>
         </router-link>
       </div>
@@ -90,12 +102,12 @@ export default {
       value: 0,
       aside: [
         { link: "", name: "熱銷商品" },
-        { link: "", name: "仿品收藏" },
-        { link: "", name: "經典畫作" },
-        { link: "", name: "典雅瓷器" },
-        { link: "", name: "書籍影視" },
-        { link: "", name: "設計小物" },
-        { link: "", name: "禮品專區" },
+        { link: "", name: "MUSES系列" },
+        { link: "", name: "聯名彩妝" },
+        { link: "", name: "典藏畫作" },
+        { link: "", name: "生活用品" },
+        { link: "", name: "精選書籍" },
+        { link: "", name: "翻玩小物" },
       ],
       produstdisplay: [
         {
@@ -119,20 +131,40 @@ export default {
           price: 400,
         },
         {
-          title: "草地上的聖母 (水彩)",
+          title: "???面膜",
           price: 300,
         },
         {
-          title: "黑帝斯筆記本",
+          title: "草地上的聖母筆記本",
           price: 1500,
         },
         {
           title: "皮革筆記本",
           price: 499,
         },
+        {
+          title: "謬思博物館迷你模型",
+          price: 1499,
+        },
+        {
+          title: "謬思聯名咖啡杯盤組",
+          price: 699,
+        },
+        {
+          title: "謬思聯名帆布袋",
+          price: 450,
+        },
+        {
+          title: "派對動物-A款",
+          price: 299,
+        },
+        {
+          title: "派對動物-B款",
+          price: 299,
+        },
       ],
       productlist: [
-        {
+      {
           title: "MUSE筆記本",
           price: 350,
         },
@@ -153,16 +185,36 @@ export default {
           price: 400,
         },
         {
-          title: "草地上的聖母 (水彩)",
+          title: "???面膜",
           price: 300,
         },
         {
-          title: "黑帝斯筆記本",
+          title: "草地上的聖母筆記本",
           price: 1500,
         },
         {
           title: "皮革筆記本",
           price: 499,
+        },
+        {
+          title: "謬思博物館迷你模型",
+          price: 1499,
+        },
+        {
+          title: "謬思聯名咖啡杯盤組",
+          price: 699,
+        },
+        {
+          title: "謬思聯名帆布袋",
+          price: 450,
+        },
+        {
+          title: "派對動物-A款",
+          price: 299,
+        },
+        {
+          title: "派對動物-B款",
+          price: 299,
         },
       ],
       searchinput: "",
@@ -189,10 +241,10 @@ export default {
 };
 </script>
 <style scoped lang="scss">
-@import "../assets/sass/style.scss";
+  @import "@/assets/sass/style.scss";
 
-h1 {
-  text-align: center;
-  font-size: 32px;
-}
+  h1 {
+    text-align: center;
+    font-size: 32px;
+  }
 </style>
