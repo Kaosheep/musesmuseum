d3d96983248cc67149359deefd576357c5f7a132
Fin7788 <cp3ru03201@gmail.com>
2023-09-20 23:20:32 +0800

    add scss

--- src/views/backend/ManageLogin.vue
+++ src/views/backend/ManageLogin.vue
@@ -1,22 +1,39 @@
 <template>
-    <form action="">
-        <label>管理員帳號</label>
-        <input type="text">
-        <label>管理員密碼</label>
-        <input type="text">
-        <button><RouterLink to="/DashBoard">登入</RouterLink></button>
-    </form>
+    <main>
+        <div>
+            <form action="">
+                <img src="../../assets/image/logo/logo.png" alt="">
+                <div>
+                    <div>
+                        <label>管理員帳號:</label>
+                        <input type="text">
+                    </div>
+                    <div>
+                        <label>管理員密碼:</label>
+                        <input type="text">
+                    </div>
+                    <div>
+                        <button>
+                            <RouterLink to="/DashBoard">登入</RouterLink>
+                        </button>
+                    </div>
+
+
+                </div>
+            </form>
+        </div>
+    </main>
 </template>
    
 <script>
 export default {
-    data(){
-       return {
-       
-       }
+    data() {
+        return {
+
+        }
     },
-    methods:{
-    
+    methods: {
+
     }
 }
 </script>
@@ -25,4 +42,55 @@ export default {
 <style scoped lang="scss">
 @import "@/assets/sass/style.scss";
 
+main {
+    width: 100vw;
+    height: 100vh;
+    // background-image: linear-gradient(to bottom right, #009CA8, #FBF796);
+    // background-image: linear-gradient(to bottom right, #000000, #ffffff);
+    justify-content: center;
+    display: flex;
+    box-shadow: inset -20px -17px 20px 17px rgba(247, 0, 0, 0.2);
+    padding: 20px;
+    border-radius: 10px;
+
+    div {
+
+        form {
+            background-color: rgba(255, 255, 255, 0.6);
+            border-radius: 10px;
+            align-items: center;
+            border-radius: 10px;
+            padding: 10px;
+            display: block;
+            color: #fff;
+
+            img {
+                display: block;
+                padding: 10px;
+                width: 50%;
+                margin: 0 auto;
+            }
+
+            div {
+                margin: 0 auto;
+                text-align: center;
+
+                div {
+                    align-items: center;
+                    margin: 10px;
+
+                    input {
+                        background-color: transparent;
+                        border: 0;
+                        box-shadow: inset 9px 2px 20px 0px rgba(247, 0, 0, 0.2);
+                        border-radius: 10px;
+                        padding: 5px;
+                        margin-top: 10px;
+                        text-align: center;
+                    }
+                }
+            }
+        }
+    }
+}
 </style>
