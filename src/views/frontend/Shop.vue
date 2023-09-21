cc41a50ebcb1be84b9e1b9ca3f6f6f3de100ed03
helilun <tracy021724@gmail.com>
2023-09-21 18:13:58 +0800

    fix shop

--- src/views/frontend/Shop.vue
+++ src/views/frontend/Shop.vue
@@ -1,11 +1,5 @@
 <template>
-  <Carousel
-    v-model="value"
-    autoplay
-    loop
-    :autoplay-speed="4000"
-    class="shop_carousel"
-  >
+  <Carousel v-model="value" autoplay loop :autoplay-speed="4000" class="shop_carousel">
     <CarouselItem class="banner">
       <img src="@/assets/image/productimage/banner/AntoinettePoudre.png" />
     </CarouselItem>
@@ -33,7 +27,7 @@
         <button class="pinkBtn" @click="prodKind = item.kind">
           {{ list.kind }}
         </button>
-      </router-link>
+      </li>
       <select class="shop_filter" v-model="sortType" @change="priceClick(sortType)">
         <option>
           商品排序
@@ -47,7 +41,9 @@
       </select>
     </aside>
     <div class="shop_container">
-      <div v-if="produstdisplay == 0">查無商品</div>
+      <div v-if="produstdisplay == 0">
+        查無商品
+      </div>
       <div class="item" v-for="(item, id) in produstdisplay" :key="id" v-else>
         <router-link to="/Home/ProductPage/:id" @click="goToProductDetail(id)">
           <div class="image">
@@ -58,18 +54,19 @@
           <span>
             <Heart>
             </Heart>
-            <router-link to="/Home/ProductPage">
-              {{item.title}}
+            <router-link to="/Home/ProductPage/:id" @click="goToProductDetail(id)">
+              <p>{{ item.title }}</p>
             </router-link>
             <font-awesome-icon :icon="['fas', 'cart-shopping']" id="car" />
           </span>
-          <router-link to="/Home/ProductPage">
+          <router-link to="/Home/ProductPage/:id" @click="goToProductDetail(id)">
             <span>
-              ${{item.price}}
+              ${{ item.price }}
             </span>
-          </router-link>   
+          </router-link>
         </div>
       </div>
+      <Page :total="getTotalPage"/>
     </div>
   </div>
 </template>
@@ -119,77 +116,90 @@ export default {
           title: "MUSES筆記本",
           price: 350,
           image: require('@/assets/image/productimage/productimage0.png'),
+          kind: ["所有商品", "辦公文具"],
         },
         {
           id: 1,
           title: "手繪紅鶴小廢包",
           price: 3000,
           image: require('@/assets/image/productimage/productimage1.png'),
+          kind: ["所有商品", "翻玩小物"],
         },
         {
           id: 2,
           title: "維納斯口紅",
           price: 2500,
           image: require('@/assets/image/productimage/productimage2.png'),
+          kind: ["所有商品", "時尚彩妝"],
         },
         {
           id: 3,
           title: "諾貝爾仿徽",
           price: 3500,
           image: require('@/assets/image/productimage/productimage3.png'),
+          kind: ["所有商品", "翻玩小物"],
         },
         {
           id: 4,
           title: "MUSES素T",
           price: 400,
           image: require('@/assets/image/productimage/productimage4.png'),
+          kind: ["所有商品", "聯名系列"],
         },
         {
           id: 5,
           title: "路易絲面膜",
           price: 300,
           image: require('@/assets/image/productimage/productimage5.png'),
+          kind: ["所有商品", "翻玩小物"],
         },
         {
           id: 6,
           title: "草地上的聖母筆記本",
           price: 1500,
           image: require('@/assets/image/productimage/productimage6.png'),
+          kind: ["所有商品", "辦公文具"],
         },
         {
           id: 7,
           title: "皮革筆記本",
           price: 499,
           image: require('@/assets/image/productimage/productimage7.png'),
+          kind: ["所有商品", "辦公文具"],
         },
         {
           id: 8,
           title: "MUSES博物館迷你模型",
           price: 1499,
-          image: require("@/assets/image/productimage/productimage8.png"),
+          image: require('@/assets/image/productimage/productimage8.png'),
         },
         {
           id: 9,
           title: "MUSES咖啡杯盤組",
           price: 699,
           image: require('@/assets/image/productimage/productimage9.png'),
+          kind: ["所有商品", "聯名系列", "生活用品"],
         },
         {
           id: 10,
           title: "MUSES帆布袋",
           price: 450,
           image: require('@/assets/image/productimage/productimage10.png'),
+          kind: ["所有商品", "聯名系列", "生活用品"],
         },
         {
           id: 11,
           title: "派對動物-A款",
           price: 299,
           image: require('@/assets/image/productimage/productimage11.png'),
+          kind: ["所有商品", "辦公文具"],
         },
         {
+          id: 12,
           title: "派對動物-B款",
           price: 299,
           image: require('@/assets/image/productimage/productimage12.png'),
+          kind: ["所有商品", "辦公文具"],
         },
       ],
       produstdislist: [
@@ -198,77 +208,90 @@ export default {
           title: "MUSES筆記本",
           price: 350,
           image: require('@/assets/image/productimage/productimage0.png'),
+          kind: ["所有商品", "辦公文具"],
         },
         {
           id: 1,
           title: "手繪紅鶴小廢包",
           price: 3000,
           image: require('@/assets/image/productimage/productimage1.png'),
+          kind: ["所有商品", "翻玩小物"],
         },
         {
           id: 2,
           title: "維納斯口紅",
           price: 2500,
           image: require('@/assets/image/productimage/productimage2.png'),
+          kind: ["所有商品", "時尚彩妝"],
         },
         {
           id: 3,
           title: "諾貝爾仿徽",
           price: 3500,
           image: require('@/assets/image/productimage/productimage3.png'),
+          kind: ["所有商品", "翻玩小物"],
         },
         {
           id: 4,
           title: "MUSES素T",
           price: 400,
           image: require('@/assets/image/productimage/productimage4.png'),
+          kind: ["所有商品", "聯名系列"],
         },
         {
           id: 5,
           title: "路易絲面膜",
           price: 300,
           image: require('@/assets/image/productimage/productimage5.png'),
+          kind: ["所有商品", "翻玩小物"],
         },
         {
           id: 6,
           title: "草地上的聖母筆記本",
           price: 1500,
           image: require('@/assets/image/productimage/productimage6.png'),
+          kind: ["所有商品", "辦公文具"],
         },
         {
           id: 7,
           title: "皮革筆記本",
           price: 499,
           image: require('@/assets/image/productimage/productimage7.png'),
+          kind: ["所有商品", "辦公文具"],
         },
         {
           id: 8,
           title: "MUSES博物館迷你模型",
           price: 1499,
-          image: require("@/assets/image/productimage/productimage8.png"),
+          image: require('@/assets/image/productimage/productimage8.png'),
         },
         {
           id: 9,
           title: "MUSES咖啡杯盤組",
           price: 699,
           image: require('@/assets/image/productimage/productimage9.png'),
+          kind: ["所有商品", "聯名系列", "生活用品"],
         },
         {
           id: 10,
           title: "MUSES帆布袋",
           price: 450,
           image: require('@/assets/image/productimage/productimage10.png'),
+          kind: ["所有商品", "聯名系列", "生活用品"],
         },
         {
           id: 11,
           title: "派對動物-A款",
           price: 299,
           image: require('@/assets/image/productimage/productimage11.png'),
+          kind: ["所有商品", "辦公文具"],
         },
         {
+          id: 12,
           title: "派對動物-B款",
           price: 299,
           image: require('@/assets/image/productimage/productimage12.png'),
+          kind: ["所有商品", "辦公文具"],
         },
       ],
       searchinput: "",
@@ -299,10 +322,10 @@ export default {
     },
     goToProductDetail(id) {
       this.$router.push({
-        name: "ProductPage",
+        name: 'ProductPage',
         params: {
           id: id,
-        },
+        }
       });
     },
     // handlePageChange(page) {
@@ -320,18 +343,23 @@ export default {
   computed: {
     produstdisplay() {
       return this.produstdisplay.slice().sort((a, b) => {
-        if(this.sortType === 'asc') {
+        if (this.sortType === 'asc') {
           return a.price - b.price;
-        } else if(this.sortType === 'desc'){
+        } else if (this.sortType === 'desc') {
           return b.price - a.price;
         }
       });
-    }
+    },
+    // catList() {
+    //   if(this.prodKind === '所有商品'){
+    //     return this.produstdisplay;
+    //   }
+    // },
   },
   mounted() {
     document.body.style.height = `auto`;
-  }
-};
+  },
+}
 </script>
 <style scoped lang="scss">
 h1 {
