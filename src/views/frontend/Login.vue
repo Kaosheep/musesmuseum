15c59d8443d263f44a4ae108b265bd0c216dd8d3
yaaa333 <chip09chip@gmail.com>
2023-09-26 20:34:07 +0800

    test php

--- src/views/frontend/Login.vue
+++ src/views/frontend/Login.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="bgcGY cardCenter">
     <main>
-      <div class="backGroundCardbBlue"></div>
-      <div class="backGroundCard">
+      <!-- <div class="backGroundCardbBlue"></div> -->
+      <div class="backGroundCard ">
         <div class="backGroundCardBtns">
           <router-link :to="a.link" v-for="a in memBtnLink">
             <button :class="[a.name === '會員登入' ? 'pinkBtnLight' : 'pinkBtn']">
@@ -34,7 +34,7 @@
           </div>
 
           <div class="memloginSubmit">
-            <input type="button" id="btnLogin" class="submitBtn" value="登入" @click="checkLoginData" />
+            <input type="submit" id="btnLogin" class="submitBtn" value="登入" @click="checkLoginData" />
             <!-- <input type="button" id="btnCancel" value="取消"> -->
           </div>
         </form>
@@ -43,7 +43,9 @@
   </div>
 </template>
 
+
 <script>
+// import $ from "jquery"; 
 import Footer from "@/components/Footer.vue";
 export default {
   components: {
@@ -68,36 +70,108 @@ export default {
       // } else {
       //   window.alert("帳密錯誤");
       // }
-      console.log('checkLoginData');
-      let input = {
-      mem_account:'charmy101@gmail.com',
-      mem_psw:'charmy101'
+      //帳密
+      // console.log('checkLoginData');
+      // let input = {
+      // mem_account:'charmy101@gmail.com',
+      // mem_psw:'charmy101'
 
-      }
-      fetch(
-        'https://tibamef2e.com/cgd103/g1/api/getConfirmMember.php',{
-        method:'POST',
-        body:new URLSearchParams(input)
-        }
-      )
-      .then(res => res.json())
-      .then(json => console.log(json))
-      // if (this.memId === "test" && this.memPsw === "test") {
-      //   window.alert("登入成功");
-      // } else {
-      //   window.alert("帳密錯誤");
       // }
+      // //帳密
+      // fetch(
+      //   'https://tibamef2e.com/cgd103/g1/api/getConfirmMember.php',{
+      //   method:'POST',
+      //   body:new URLSearchParams(input)
+      //   }
+      // )
+      // .then(res => res.json())
+      // .then(json => console.log(json))
+     
 
     },
   },
   mounted() {
+    // function $id(id){
+    //   return document.getElementById(id);
+    // }
+    // function showLoginForm(){
+    //   if ($id('spanLogin').innerHTML == "登入"){
+    //     $id('lightBox').style.display = 'block';
+    //   } else {
+    //     $.ajax({
+    //       url:"http://localhost:8080/musesmuseum/public/phps/login.php",
+    //       dataType: "json",
+    //       success(response) { //response為已parse完的結果
+    //       // member = response;
+    //       if( member.memId) {
+    //         $id("memName").innerText = member.memName;
+    //         $id("spanLogin").innerText = "登出";
+    //       } else {
+    //         alert("帳密錯誤~");
+    //       }
+    //     },
+    //     })
+    //   }
+    // }
     document.body.style.height = `auto`;
   }
 };
+
+// function sendForm() {
+//   //-----------------------------------使用Ajax 回server端,取回登入者姓名, 放到頁面上    
+//   let memId = $id("memId").value;
+//   let memPsw = $id("memPsw").value;
+//   // let URL = ".";//當前資料夾
+//   let URL = "http://localhost/musesmuseum/public/phps/login.php"; //for打包時
+//   $.ajax({
+//     // url: "login.php", //上線階段用這個
+//     url: `${URL}/ajaxLogin.php`,
+//     //改成阿帕契localhost網址連結 //記得在php檔 加允許讀取
+//     //開發階段用這個↓
+//     url:"http://localhost:8080/musesmuseum/public/phps/login.php",
+//     method: "post",
+//     contentType: "application/x-www-form-urlencoded",
+//     data: {memId, memPsw},
+//     dataType: "json",
+//     success(response) { //response為已parse完的結果
+//       member = response;
+//       if( member.memId) {
+//         $id("memName").innerText = member.memName;
+//         $id("spanLogin").innerText = "登出";
+//       } else {
+//         alert("帳密錯誤~");
+//       }
+//     },
+//     error(xhr, status, error) {
+//       console.log(status, error);
+//     }
+//   });
+  //-----------------------------------
+
+
+// }
+
+// window.addEventListener("load", function () {
+//   //===設定spanLogin.onclick 事件處理程序是 showLoginForm
+
+//   $id('spanLogin').onclick = showLoginForm;
+
+//   //===設定btnLogin.onclick 事件處理程序是 sendForm
+//   $id('btnLogin').onclick = sendForm;
+
+//   //===設定btnLoginCancel.onclick 事件處理程序是 cancelLogin
+//   $id('btnLoginCancel').onclick = cancelLogin;
+
+// }, false);
+
+
+
+
+
 </script>
 <style scoped lang="scss">
 .backGroundCard{
- 
+  @include dotfm();
   form{
       width:auto;
       margin:auto 213px;
