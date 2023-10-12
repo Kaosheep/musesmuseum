cf74915f58ab26591ab18f813861a669e2ad747c
Kaosheep <littlekaotw@gmail.com>
2023-10-12 11:36:41 +0800

    fixupdateimg

--- public/phps/news_updateupload.php
+++ public/phps/news_updateupload.php
@@ -6,35 +6,44 @@ header('Content-Type: application/json');
 
 
 try {
-    switch ($_FILES["image"]["error"]) {
 
-        case UPLOAD_ERR_OK :
-            $dir = "../image/news/";
-            if( !file_exists($dir) ){
-                mkdir($dir);
-            }
-                    
-            $from = $_FILES["image"]["tmp_name"];
-        
-            $filename = basename($_FILES['image']['name']);
-        
-            $to = $dir . basename($_FILES['image']['name']);
-            copy($from, $to);
-            echo json_encode("更新成功");    
-            break;
-        case UPLOAD_ERR_INI_SIZE :
-            echo json_encode("上傳檔案太大, 不得超過", ini_get("upload_max_filesize"));
-            break;
-        case UPLOAD_ERR_FORM_SIZE :
-            echo json_encode("上傳檔案太大, 不得超過", $_POST["MAX_FILE_SIZE"], "位元組");
-            break;
-        case UPLOAD_ERR_PARTIAL :
-            echo json_encode("上傳檔案不完整, 請再試一次");
-            break;
-        case UPLOAD_ERR_NO_FILE :
-            echo json_encode("檔案未選");
-            break;
+    if(isset($_FILES["image"])){
+        switch ($_FILES["image"]["error"]) {
+
+            case UPLOAD_ERR_OK :
+                $dir = "../image/news/";
+                if( !file_exists($dir) ){
+                    mkdir($dir);
+                }
+                        
+                $from = $_FILES["image"]["tmp_name"];
+            
+                $filename = basename($_FILES['image']['name']);
+            
+                $to = $dir . basename($_FILES['image']['name']);
+                copy($from, $to);
+                echo json_encode("更新成功");    
+                break;
+            case UPLOAD_ERR_INI_SIZE :
+                echo json_encode("上傳檔案太大, 不得超過", ini_get("upload_max_filesize"));
+                break;
+            case UPLOAD_ERR_FORM_SIZE :
+                echo json_encode("上傳檔案太大, 不得超過", $_POST["MAX_FILE_SIZE"], "位元組");
+                break;
+            case UPLOAD_ERR_PARTIAL :
+                echo json_encode("上傳檔案不完整, 請再試一次");
+                break;
+            case UPLOAD_ERR_NO_FILE :
+                $filename = $_POST["image"];
+                break;
+        }
+    }else{
+        $filename = $_POST["image"];
+        echo json_encode("更新成功"); 
     }
+        
+  
+
  //連線
  require_once("./connectMuses.php");
     
